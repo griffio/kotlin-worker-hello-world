23ff709e30b7a2cfd5dce1d47b4e76f6f3be40aa
griffio <griffio@users.noreply.github.com>
2020-10-25 15:41:56 +0000

    ServiceWorker
    Implement as ServiceWorker

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,29 @@
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.promise
+import org.w3c.fetch.Request
 import org.w3c.fetch.Response
 import org.w3c.fetch.ResponseInit
 import org.w3c.workers.FetchEvent
-import kotlin.js.Promise
+import org.w3c.workers.ServiceWorkerGlobalScope
+// Cloudflare workers are compatible with Service Workers
+external val self: ServiceWorkerGlobalScope
 
-external fun addEventListener(type: String, f: (FetchEvent) -> Unit)
+val global = MainScope()
 
 fun main() {
-    addEventListener("fetch") { event: FetchEvent ->
-        val headers: dynamic = object {}
-        headers["content-type"] = "text/plain"
-        event.respondWith(
-            Promise.resolve(
-                Response(
-                    "Kotlin Worker hello world",
-                    ResponseInit(headers = headers)
-                )
-            )
-        )
-    }
+    self.addEventListener("fetch", { event ->
+        event as FetchEvent
+
+        global.promise {
+            helloWorld(event.request)
+        }.let { event.respondWith(it) }
+
+    })
+}
+
+fun helloWorld(request: Request): Response {
+    console.log(request)
+    val headers: dynamic = object {}
+    headers["content-type"] = "text/plain"
+    return Response("Hello World", ResponseInit(headers = headers))
 }
