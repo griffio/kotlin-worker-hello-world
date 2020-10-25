249b597c7ba5eb33963aa3ba3d9d6802810e5b4d
griffio <griffio@users.noreply.github.com>
2020-10-25 16:43:06 +0000

    Streamline promise

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,29 +1,24 @@
-import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.promise
 import org.w3c.fetch.Request
 import org.w3c.fetch.Response
 import org.w3c.fetch.ResponseInit
 import org.w3c.workers.FetchEvent
 import org.w3c.workers.ServiceWorkerGlobalScope
+
 // Cloudflare workers are compatible with Service Workers
 external val self: ServiceWorkerGlobalScope
-
-val global = MainScope()
 
 fun main() {
     self.addEventListener("fetch", { event ->
         event as FetchEvent
-
-        global.promise {
-            helloWorld(event.request)
-        }.let { event.respondWith(it) }
-
+        helloWorld(event.request).let { event.respondWith(it) }
     })
 }
 
-fun helloWorld(request: Request): Response {
+fun helloWorld(request: Request) = GlobalScope.promise {
     console.log(request)
     val headers: dynamic = object {}
     headers["content-type"] = "text/plain"
-    return Response("Hello World", ResponseInit(headers = headers))
+    Response("Hello World", ResponseInit(headers = headers))
 }
